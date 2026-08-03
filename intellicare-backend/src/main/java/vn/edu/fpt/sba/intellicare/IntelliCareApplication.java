936f6a5e114932e6a2a9ba3fc9a8b5f260812902
Tai Nguyen <tainh.ce190284@gmail.com>
2026-08-03 19:16:34 +0700

    fix: force JVM timezone to Asia/Ho_Chi_Minh to fix 7-hour offset in timestamps

--- intellicare-backend/src/main/java/vn/edu/fpt/sba/intellicare/IntelliCareApplication.java
+++ intellicare-backend/src/main/java/vn/edu/fpt/sba/intellicare/IntelliCareApplication.java
@@ -3,8 +3,14 @@ package vn.edu.fpt.sba.intellicare;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class IntelliCareApplication {
+
+    static {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IntelliCareApplication.class, args);
